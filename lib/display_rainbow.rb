cd7bb9cf1db3f1f1c25487f205cf1c906267f03e
Alie Stone <helloaliestone@gmail.com>
2020-09-03 19:53:20 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,4 @@
 # This is a method for displaying a rainbow 
 
-colors = "['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']"
-puts "colors": #
-def display_rainbow
-  puts "The colors in the rainbow are: #{colors[0,1,2,3,4,5,6]}"
-end
-display_rainbow
+colors =
+[]
