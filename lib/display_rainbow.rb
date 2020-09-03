48a830662933dd4cd6635f9eeebc323d1c895878
Alie Stone <helloaliestone@gmail.com>
2020-09-03 19:35:25 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,3 @@
-# Write your #display_rainbow method here
+# This is a method for displaying a rainbow 
+
+def display_rainbow colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
