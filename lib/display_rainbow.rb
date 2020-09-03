e533a5adc09898a5ce9ea685d304be5fbb3dd1f2
Alie Stone <helloaliestone@gmail.com>
2020-09-03 19:59:31 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,8 @@
 
 colors =
 ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
-puts "The colors of the raimnow are: #{colors[0,1,2,3,4,5,6]}"
-end
+puts "The colors of the rainbow are: #{colors[0,1,2,3,4,5,6]}"
+display_rainbow =
+puts 
 
 print colors 
