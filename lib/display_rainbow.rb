189e7f37668bcbb5626476d072a45d4a9c9c74ac
Alie Stone <helloaliestone@gmail.com>
2020-09-03 20:15:16 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,9 @@
 # This is a method for displaying a rainbow 
 
-colors =
-['R', 'O', 'Y', 'G', 'B', 'I', 'V']
 
-def display_rainbow
-  puts "{colors [0,1,2,3,4,5,6]}"
 
-display_rainbow
+def display_rainbow(colors)
+  puts "R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+
 end
 
