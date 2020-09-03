2013dbd54c4eefd2db2f8105bd4d9416567e031f
Alie Stone <helloaliestone@gmail.com>
2020-09-03 19:55:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,6 @@
 # This is a method for displaying a rainbow 
 
 colors =
-[]
+['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+puts "The colors of the raimnow are: #{colors[0,1,2,3,4,5,6]}"
+end
